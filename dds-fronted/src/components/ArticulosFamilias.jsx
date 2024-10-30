24c3abbc0a0882bed9b564b3140c07e1ca9adbab
Jennifer Lopez <89511@sistemas.frc.utn.edu.ar>
2024-10-30 00:24:18 -0300

    Etapa 2 _ Frontend

--- dds-fronted/src/components/ArticulosFamilias.jsx
+++ dds-fronted/src/components/ArticulosFamilias.jsx
@@ -1,29 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import { articulosFamiliasMockService } from '../services/articulosFamilias-mock-service';
 function ArticulosFamilias() {
-    return (
-      <>
-        <div className="tituloPagina">Articulos Familias</div>
-        <div className="table-responsive">
-           <table className="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th style={{ width: "40%" }}>IdArticuloFamilia</th>
-                <th style={{ width: "60%" }}>Nombre</th>
+  const tituloPagina = 'ArticulosFamilias';
+  const [articulosFamilias, setArticulosFamilias] = useState(null);
+  // cargar al montar el componente (solo una vez)
+  useEffect(() => {
+    BuscarArticulosFamilas();
+  }, []);
+  async function BuscarArticulosFamilas() {
+    let data = await articulosFamiliasMockService.Buscar();
+    setArticulosFamilias(data);
+  };
+  return (
+    <div>
+      <div className="tituloPagina">{tituloPagina}</div>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th style={{ width: "40%" }}>IdArticuloFamilia</th>
+            <th style={{ width: "60%" }}>Nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articulosFamilias &&
+            articulosFamilias.map((articulofamilia) => (
+              <tr key={articulofamilia.IdArticuloFamilia}>
+                <td>{articulofamilia.IdArticuloFamilia}</td>
+                <td>{articulofamilia.Nombre}</td>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Accesorios</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Audio</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </>
-    );
-  }
-  export { ArticulosFamilias };
-  
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export {ArticulosFamilias};
